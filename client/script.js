686efffa2be4c537b96c170ee56f7bf0d46346a5
zeyutt <zeyutt@163.com>
2023-02-10 16:49:38 +0800

    update server url

--- client/script.js
+++ client/script.js
@@ -88,7 +88,7 @@ const handleSubmit = async (e) => {
 
     //fetch data from server
     // https://codex-im0y.onrender.com/
-    const response = await fetch('http://localhost:5000', {
+    const response = await fetch('https://robotic-codex.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
